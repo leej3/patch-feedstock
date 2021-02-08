e5688559ba2bb137bd8ddeecee38b1bae95e5526
Ilan Schnell <ilanschnell@gmail.com>
2021-02-08 17:53:54 -0600

    remove python test dependency

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,16 +1,14 @@
 import subprocess
-from os.path import isfile
 
 command = 'patch'
 testfile = 'testfile.patch'
 
 subprocess.check_call([command, '--help'])
 
-if isfile(testfile):
-    subprocess.check_call([command, '-i', "testfile.patch"])
-    with open("testfile") as f1:
-        testfile1 = f1.read()
-    with open("testfile2") as f2:
-        testfile2 = f2.read()
+subprocess.check_call([command, '-i', "testfile.patch"])
+with open("testfile") as f1:
+    testfile1 = f1.read()
+with open("testfile2") as f2:
+    testfile2 = f2.read()
 
-    assert testfile1 == testfile2
+assert testfile1 == testfile2

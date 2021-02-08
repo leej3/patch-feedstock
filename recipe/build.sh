73b7514cf7c231692145e83d8441f72ee4cad4fe
Ilan Schnell <ilanschnell@gmail.com>
2021-02-08 17:19:44 -0600

    fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build-aux
+
 ./configure --prefix="${PREFIX}" --host=${HOST}
 make -j${CPU_COUNT} ${VERBOSE_AT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+    make check
 fi
 make install
